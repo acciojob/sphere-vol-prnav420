33bdda401fac8a7dcd3effb0f7604ee1e17a35e4
prnav420 <118669489+prnav420@users.noreply.github.com>
2023-06-01 11:15:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function volume_sphere() {
-    //Write your code here
-  
-} 
+  //Write your code here
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+  let radiuss = document.getElementById("radius").value;
+  let volumes = document.getElementById("volume");
+
+  if (radiuss.length > 0) {
+    volumes.value = (4 / 3) * 3.14285714286 * Math.pow(radiuss,3);
+  }
+}
+
+window.onload = function () {
+  document.getElementById("MyForm").onsubmit = function (event) {
+    event.preventDefault(); // Prevent form submission
+    volume_sphere();
+  };
+};
+
+
